ab74532febda3366c3619e74bef181df0fbbe897
Richa Khatod <richa.khatod@lincode.ai>
2026-08-15 10:26:22 +0530

    feat: create view point for account serializer

--- ledger/views.py
+++ ledger/views.py
@@ -1,8 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ledger.utils import *
+from rest_framework.generics import ListCreateAPIView
+from .models import Account
+from .serializers import AccountSerializer
 
 @api_view(['GET'])
 def health_point_check(request):
     message = health_point_check_util(request)
     return Response(({'message': message}))
+
+
+class AccountListCreateView(ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
